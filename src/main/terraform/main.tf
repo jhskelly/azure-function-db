13f2f0714e5e7b5b6351e9ea20fdec9904d195f7
James Kelly <jhkelly05@gmail.com>
2024-08-05 23:53:46 +0100

    Numerous changes

--- src/main/terraform/main.tf
+++ src/main/terraform/main.tf
@@ -21,35 +21,25 @@ resource "azurerm_storage_container" "shows" {
   container_access_type = "private"
 }
 
-resource "azurerm_app_service_plan" "shows" {
-  name                = "asp-shows"
-  location            = azurerm_resource_group.shows.location
+resource "azurerm_service_plan" "shows" {
+  name                = "shows_sp"
   resource_group_name = azurerm_resource_group.shows.name
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
+  location            = azurerm_resource_group.shows.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
 }
 
-resource "azurerm_function_app" "shows" {
+resource "azurerm_linux_function_app" "shows" {
   name                       = "app-shows"
   location                   = azurerm_resource_group.shows.location
   resource_group_name        = azurerm_resource_group.shows.name
-  app_service_plan_id        = azurerm_app_service_plan.shows.id
+  service_plan_id        = azurerm_service_plan.shows.id
   storage_account_name       = azurerm_storage_account.shows.name
   storage_account_access_key = azurerm_storage_account.shows.primary_access_key
-  os_type                    = "linux"
-  runtime_stack              = "java"
-  version                    = "~3"
-
-  app_settings = {
-    FUNCTIONS_WORKER_RUNTIME = "java"
-    JAVA_VERSION             = "11"
-    WEBSITE_RUN_FROM_PACKAGE = "1"
-  }
 
   site_config {
-    always_on                = true
-    java_version             = "11"
+    application_stack {
+      java_version = "17"
+    }
   }
 }
